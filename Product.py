c947161e27db1ef42bbcae79da93654772260bad
Henry Hsieh <lucky9456@msn.com>
2021-07-11 17:21:19 +0800

    1.1

--- Product.py
+++ Product.py
@@ -5,4 +5,17 @@ Created on Sun Jul 11 17:01:20 2021
 
 @author: juicy
 """
+products = []
+while True:
+    name = input('請輸入商品名稱')
+    if name == 'q':
+        break
+    price = input('請輸入價格')
+    # p = []
+    # p.append(name)
+    # p.append(price)
+    p = [name, price]
+    products.append(p)
+print(products)
 
+print(products[0][0])
