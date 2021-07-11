802565572d1172f4fdf212be1f99dfbea5f9f168
Henry Hsieh <lucky9456@msn.com>
2021-07-12 01:48:36 +0800

    1.3

--- Product.py
+++ Product.py
@@ -5,7 +5,18 @@ Created on Sun Jul 11 17:01:20 2021
 
 @author: juicy
 """
+
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue
+        name, price = line.strip().split(',')
+        products.append([name, price])
+
+for p in products:
+    print(p)
+
 while True:
     name = input('請輸入商品名稱')
     if name == 'q':
@@ -16,9 +27,13 @@ while True:
     # p.append(price)
     # p = [name, price]
     products.append([name, price])
-print(products)
 
-print(products[0][0])
 
+    
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+    f.write('商品,價格\n')
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
+        
 for p in products:
     print(p)
